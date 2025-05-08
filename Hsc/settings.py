89c4420d8ef51d976d4f68bacd55fa1ccf4678d2
Chiripio <ed.guerreros@duocuc.cl>
2025-05-08 13:40:48 -0400

    Conexión configurada a PostgreSQL con dj-database-url

--- Hsc/settings.py
+++ Hsc/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,10 +99,7 @@ import os
 os.environ["TNS_ADMIN"] = str(BASE_DIR / "wallet" / "network" / "admin")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
